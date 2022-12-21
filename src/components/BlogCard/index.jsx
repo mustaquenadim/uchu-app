379c908130467a3c6f9f75be71862f32bfb4eb59
Mustaque Nadim <mustaquenadim2000@gmail.com>
2022-12-21 12:55:02 +0600

    blog card 1 issue fixed

--- src/components/BlogCard/index.jsx
+++ src/components/BlogCard/index.jsx
@@ -3,23 +3,25 @@ import { Link } from 'react-router-dom';
 
 const BlogCard = ({ blog }) => {
 	return (
-		<Link to={`/article/${blog._id}`}>
-			<div className="col">
-				<div className="card h-100 rounded-0">
-					<img
-						src={blog?.imageURL}
-						className="card-img-top"
-						alt={`${blog?.title}`}
-					/>
-					<div className="card-body">
+		<div className="col">
+			<div className="card h-100 rounded-0">
+				<img
+					src={blog?.imageURL}
+					className="card-img-top"
+					alt={`${blog?.title}`}
+				/>
+				<div className="card-body">
+					<Link to={`/article/${blog._id}`}>
 						<h5 className="card-title">{blog.title}</h5>
-						<p className="card-text">
-							<small className="text-muted">Last updated 3 mins ago</small>
-						</p>
-					</div>
+					</Link>
+					<p className="card-text">
+						<small className="text-muted">
+							{new Date(blog?.dateTime).toLocaleDateString()}
+						</small>
+					</p>
 				</div>
 			</div>
-		</Link>
+		</div>
 	);
 };
 
